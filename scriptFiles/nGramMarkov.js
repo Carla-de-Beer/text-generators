ca5d153f90a1a1f5e36f0f3f2379b6569d866e82
Carla <carla.debeer.uk@gmail.com>
2016-10-31 21:02:54 +0000

    Adding sliders to n-Gram Markov page; adding 'clear' buttons to each page

--- scriptFiles/nGramMarkov.js
+++ scriptFiles/nGramMarkov.js
@@ -1,65 +1,70 @@
 var order = 3;
-var num = 100;
+var num = 50;
 var ngrams = {};
 var beginnings = [];
+var textString;
 var button;
-var text;
 var colors = ["color1", "color2", "color3"];
 
 function preload() {
-  text = loadStrings("sourceFiles/Dracula.txt");
-  console.log(text);
+	textString = loadStrings("sourceFiles/Spaceman on a Spree.txt");
+  	//console.log(textString);
 }
 
 function setup() {
-	"use strict";
 	noCanvas();
-
-  for (var j = 0; j < text.length; ++j) {
-    var txt = text[j];
-    for (var i = 0; i <= txt.length - order; ++i) {
-      var gram = txt.substring(i, i + order);
-
-      if (i === 0) {
-        beginnings.push(gram);
-      }
-
-      if (!ngrams[gram]) {
-        ngrams[gram] = [];
-      }
-        ngrams[gram].push(txt.charAt(i + order));
-    }
-  }
-
-  console.log(ngrams);
-  button = createButton("Generate " + order.toString() + "-Gram Markov chain");
-  button.mousePressed(generateMarkov);
-	button.addClass("btnClass");
-
+	createGrams();
 }
 
-/*function draw() {
-	order = document.getElementById("nGramRange");
-	num = document.getElementById("numRange");
-}*/
+function readFromHTML() {
+	order = document.getElementById("nGramRange").value;
+	num = document.getElementById("numRange").value;
+
+	document.getElementById("labelNGram").innerHTML = "n-Gram value: " + order;
+	document.getElementById("labelNum").innerHTML = "Output length (number of characters): " + num;
+}
+
+function createGrams() {
+
+  	for (var j = 0; j < textString.length; ++j) {
+		var txt = textString[j];
+		for (var i = 0; i <= txt.length - order; ++i) {
+		  var gram = txt.substring(i, i + order);
+
+		  if (i === 0) {
+			beginnings.push(gram);
+		  }
+
+		  if (!ngrams[gram]) {
+			ngrams[gram] = [];
+		  }
+		  ngrams[gram].push(txt.charAt(i + order));
+		}
+  	}
+
+  	//console.log(ngrams);
+}
 
 function generateMarkov() {
-	"use strict";
-  var currentGram = random(beginnings);
-  var result = currentGram;
+  	"use strict";
 
-  for (var i = 0; i < num; ++i) {
-    var possibilities = ngrams[currentGram];
-    if(!possibilities) {
-      break;
-    }
-    var next = random(possibilities);
-    result += next;
-    var len = result.length;
-    currentGram = result.substring(len - order, len);
-  }
+	readFromHTML();
+	createGrams();
 
-  var rand = random(colors);
-  createP(result).addClass(rand);
+  	var currentGram = random(beginnings);
+  	var result = currentGram;
+
+  	for (var i = 0; i < num; ++i) {
+		var possibilities = ngrams[currentGram];
+		if(!possibilities) {
+		  break;
+		}
+		result += random(possibilities);
+		var len = result.length;
+		currentGram = result.substring(len - order, len);
+  	}
+
+	var rand = random(colors);
+	createP(result).addClass(rand);
 
 }
